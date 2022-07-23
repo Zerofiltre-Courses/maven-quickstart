df8d484485b21c5cd63f969a36334aed950a6794
Philippe GUEMKAM SIMO <philippechampion58@gmail.com>
2022-07-23 22:42:30 +0200

    initialize project

---
+++ src/main/java/tech/zerofiltre/maven/App.java
@@ -0,0 +1,13 @@
+package tech.zerofiltre.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
